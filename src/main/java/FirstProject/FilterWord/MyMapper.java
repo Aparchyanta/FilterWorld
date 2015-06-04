2eb8b53c37eaf2405e3cc5db4f0327b59d538780
aparchyanta <aparchyanta.aneja@gmail.com>
2015-06-04 11:49:43 -0700

    Final Filter Word

--- src/main/java/FirstProject/FilterWord/MyMapper.java
+++ src/main/java/FirstProject/FilterWord/MyMapper.java
@@ -3,21 +3,22 @@ package FirstProject.FilterWord;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MyMapper extends Mapper<Text, IntWritable, Text, Text>{
+public class MyMapper extends Mapper<LongWritable, Text, Text, Text>{
 @Override
-protected void map(Text key, IntWritable value,
-		Mapper<Text, IntWritable, Text, Text>.Context context)
+protected void map(LongWritable key, Text value,
+		Mapper<LongWritable, Text, Text, Text>.Context context)
 		throws IOException, InterruptedException {
 	// TODO Auto-generated method stub
 	String line=value.toString();
 	String[] arr=line.split(",");
 	String id=arr[0];
-	String location=arr[1];
-	String name=arr[2];
-	if(location.equals(location)){
+	String location=arr[2];
+	String name=arr[1];
+	if(location.equals("CA")){
 		context.write(new Text(id.concat(name)), new Text(location));
 	}
 }
